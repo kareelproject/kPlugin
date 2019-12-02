78aea6f518d5b03a55c757ffd120e6f9898659f1
kareelproject <52936751+kareelproject@users.noreply.github.com>
2019-12-02 09:03:04 +0700

    Update KoBlognasnanks.js

--- KoBlognasnanks.js
+++ KoBlognasnanks.js
@@ -1,3 +1,18 @@
+if(sessionStorage.getItem("KoBlogSLN")!==null){if(sessionStorage.getItem("KoBlogSLN")=="NIGHT"){KoBlogSLN('NIGHT');}}
+if(sessionStorage.getItem("KoBlogSwitch")!==null){if(sessionStorage.getItem("KoBlogSwitch")=="GRID"){KoBlogSwitch('grid');}if(sessionStorage.getItem("KoBlogSwitch")=="LIST"){KoBlogSwitch('list');}}
+
+function NavMenuKoblog(x) {if (x.matches) {document.getElementById("koblog-navigasi").style.display = 'none';$(document).on('click', '#koblog-navigasi-button', function(){var $this = $(this), x = document.getElementById("koblog-navigasi");if(x.style.display == "none"){$('#koblog-navigasi').css('display','block');$('#koblog-navigasi').addClass('show');}else{$('#koblog-navigasi').removeClass('show');$('#koblog-navigasi').css('display','none');}});$('.col-md-3').css('flex','unset').css('max-width','unset').css('-ms-flex','unset');$('#koblog-navigasi-button').removeClass('d-md-none');$('.koblog-sidebar-right').css('height','unset').css('margin-bottom','10px');} else {}}
+function KoBlogSLN(pilih){
+var pilihan;if(pilih.toLowerCase()==""||pilih.toLowerCase()==null||pilih.toLowerCase()==undefined){pilihan='grid';}else{if(pilih.toLowerCase()=="light"){pilihan='light';}else{pilihan='night';}}
+if(pilihan=='light'){
+if(sessionStorage.getItem("KoBlogSLN")!==null){sessionStorage.removeItem("KoBlogSLN");}
+$("body").removeClass('KoBlogSN');
+}
+if(pilihan=='night'){
+if(sessionStorage.getItem("KoBlogSLN")!==null){sessionStorage.removeItem("KoBlogSLN");}sessionStorage.setItem("KoBlogSLN", "NIGHT");
+$("body").addClass('KoBlogSN');
+}
+}
 function KoBlogSwitch(pilih){
 var pilihan;if(pilih.toLowerCase()==""||pilih.toLowerCase()==null||pilih.toLowerCase()==undefined){pilihan='grid';}else{if(pilih.toLowerCase()=="list"){pilihan='list';}else{pilihan='grid';}}
 if(pilihan=='grid'){
@@ -25,18 +40,17 @@ for(var list=document.querySelectorAll("div#info-list"),i=0;i<list.length;i++){$
 for(var list=document.querySelectorAll("div#desc-list"),i=0;i<list.length;i++){$(list[i]).removeClass('d-none');}
 }
 }
+!function(){for(var aTags=document.querySelectorAll("a[data-href]"),i=0;i<aTags.length;i++){var randlinknya=aTags[i];aTags[i].addEventListener("click",function(a){var t=a.target;console.log(t),window.open(t.getAttribute("data-href"),"_blank","toolbar=0,status=0,width=800,height=600")})}}();
+!function(){var xNavMenuKoblog = window.matchMedia("(max-width: 1199px)");NavMenuKoblog(xNavMenuKoblog);xNavMenuKoblog.addListener(NavMenuKoblog);}();
 $(document).ready(function(){
 $(document).on('click','#gantiLIST', function(){KoBlogSwitch('list');});
 $(document).on('click','#gantiGRID', function(){KoBlogSwitch('grid');});
-if(sessionStorage.getItem("KoBlogSwitch")!==null){if(sessionStorage.getItem("KoBlogSwitch")=="GRID"){KoBlogSwitch('grid');}if(sessionStorage.getItem("KoBlogSwitch")=="LIST"){KoBlogSwitch('list');}}
-$(document).on('click','#KoBlogSN', function(){$("body").toggleClass('KoBlogSN');});
 });
-
-$(document).ready(function(){$(document).on('click', '#KoBlogDropN', function(){if($('#KoBlogShowN').hasClass('koblog-sidenav')){$('#KoBlogShowN').removeClass('koblog-sidenav');}else{$('#KoBlogShowN').addClass('koblog-sidenav');}});});
-function NavMenuKoblog(x) {if (x.matches) {document.getElementById("koblog-navigasi").style.display = 'none';$(document).on('click', '#koblog-navigasi-button', function(){var $this = $(this), x = document.getElementById("koblog-navigasi");if(x.style.display == "none"){$('#koblog-navigasi').css('display','block');$('#koblog-navigasi').addClass('show');}else{$('#koblog-navigasi').removeClass('show');$('#koblog-navigasi').css('display','none');}});$('.col-md-3').css('flex','unset').css('max-width','unset').css('-ms-flex','unset');$('#koblog-navigasi-button').removeClass('d-md-none');$('.koblog-sidebar-right').css('height','unset').css('margin-bottom','10px');} else {}}
-!function(){var xNavMenuKoblog = window.matchMedia("(max-width: 1199px)");NavMenuKoblog(xNavMenuKoblog);xNavMenuKoblog.addListener(NavMenuKoblog);}();
+$(document).ready(function(){
+$(document).on('click','#KoBlogSN', function(){if($('body').hasClass('KoBlogSN')==false){KoBlogSLN('night');}else{KoBlogSLN('light');}});
+$(document).on('click', '#KoBlogDropN', function(){if($('#KoBlogShowN').hasClass('koblog-sidenav')){$('#KoBlogShowN').removeClass('koblog-sidenav');}else{$('#KoBlogShowN').addClass('koblog-sidenav');}});
+});
 $(document).ready(function(){if("www.kareel.com"==SettingKGUARDIAN.licenseCode){""!=function(e){for(var n=e+"=",t=decodeURIComponent(document.cookie).split(";"),i=0;i<t.length;i++){for(var o=t[i];" "==o.charAt(0);)o=o.substring(1);if(0==o.indexOf(n))return o.substring(n.length,o.length)}return""}("KareelShield")&&$(SettingKGUARDIAN.SELECTOR).css("display","none");var e=0;$(document).on("click",SettingKGUARDIAN.SELECTOR,function(n){++e>SettingKGUARDIAN.jumlahBolehKlik&&($(SettingKGUARDIAN.SELECTOR).css("display","none"),function(e,n,t){var i=new Date;i.setTime(i.getTime()+24*t*60*60*1e3);var o="expires="+i.toGMTString();document.cookie=e+"="+n+";"+o+";path=/"}("KareelShield",e,SettingKGUARDIAN.waktuKarantina))}),$(document).on("contextmenu",SettingKGUARDIAN.SELECTOR,function(e){return!1})}});
-
 var asnHAJSN2733N=$('footer>.d-table>.d-table-row>.d-table-cell>.container>.row>.col-sm-7>a');
 if (asnHAJSN2733N.length&&asnHAJSN2733N.is(':hidden')==false) {
 $('footer').css('display','block').css('width','100%').css('position','relative').css('height','50px');
@@ -45,4 +59,3 @@ $('#konetenhalaman').removeClass('d-none');$('#konetenhalamanLOAD').addClass('d-
 }else{
 setTimeout(function(){location.href='http://www.kareel.com';}, 5000);$('footer').html('<div class="modal fade" id="ShowCopyright" tabindex="-1" role="dialog" aria-labelledby="ShowCopyrightTitle" aria-hidden="true"><div class="modal-dialog modal-dialog-centered" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title text-danger" id="ShowCopyrightTitle">Copyright Notice</h5></div><div class="modal-body">Anda melihat pemberitahuan ini berarti anda telah merubah hak cipta yang tidak seharusnya boleh dirubah.<br/></div><div class="modal-footer">Oleh <strong>Kareel Project</strong><small class="text-success">www.kareel.com</small></div></div></div></div>');setTimeout(function(){$('#ShowCopyright').modal('show');}, 1000);
 }
-!function(){for(var aTags=document.querySelectorAll("a[data-href]"),i=0;i<aTags.length;i++){var randlinknya=aTags[i];aTags[i].addEventListener("click",function(a){var t=a.target;console.log(t),window.open(t.getAttribute("data-href"),"_blank","toolbar=0,status=0,width=800,height=600")})}}();
